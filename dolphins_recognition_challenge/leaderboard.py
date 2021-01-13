381e3190c871dc4b553b899e43f3ebee29baa0b0
Matija Krivošić <matija@airt.ai>
2021-01-13 14:13:22 +0000

    wip: leaderboard

--- dolphins_recognition_challenge/leaderboard.py
+++ dolphins_recognition_challenge/leaderboard.py
@@ -5,10 +5,105 @@ __all__ = ['get_leaderboard']
 # Internal Cell
 
 import pandas as pd
+import numpy as np
+import boto3
+from pathlib import Path
+
+# Internal Cell
+
+import zipfile
+import shutil
+import torch
+import tempfile
+
+from .datasets import get_dataset
+from .instance_segmentation.model import *
+
+# Internal Cell
+
+
+def parse_filename(fname):
+    tmp = fname.split("-")
+    date = pd.to_datetime(tmp[1] + tmp[2] + tmp[3])
+    alias = tmp[6]
+    email = tmp[7]
+    submitted_iou = tmp[5].split("=")[1]
+
+    return {
+        "file_name": fname,
+        "date": date,
+        "alias": alias,
+        "email": email,
+        "submitted_iou": submitted_iou,
+        "calculated_iou": np.nan,
+    }
+
+# Internal Cell
+
+s3 = boto3.resource("s3")
+my_bucket = s3.Bucket("ai-league.cisex.org")
+private_leaderboard_path = Path("private_leaderboard.csv")
+public_leaderboard_path = Path("leaderboard.csv")
+
+
+def get_submissions_from_s3(private_leaderboard_path=private_leaderboard_path):
+    """Downloads the zip file from s3 if there is no record of it in the csv file"""
+    if private_leaderboard_path.exists():
+        private_leaderboard = pd.read_csv(private_leaderboard_path)
+    else:
+        private_leaderboard = dict(file_name=[])
+
+    # download file into models_for_evaluation directory
+    s3_objects = [
+        s3_object
+        for s3_object in my_bucket.objects.all()
+        if Path(s3_object.key).match("*submission*.zip")
+        and Path(s3_object.key).name not in private_leaderboard["file_name"]
+    ]
+    for i, s3_object in enumerate(s3_objects):
+        print(f"Downloading {i+1}/{len(s3_objects)} from S3...")
+        my_bucket.download_file(s3_object.key, f"models_for_evaluation/{Path(s3_object.key).name}")
+
+    # return new entries
+    new_entries = pd.Series([Path(s3_object.key).name for s3_object in s3_objects]).apply(parse_filename).apply(pd.Series)
+    return new_entries
+
+
+# Internal Cell
+
+def public(private_leaderboard):
+    return private_leaderboard[["alias", "date", "submitted_iou", "calculated_iou"]]
+
+# Internal Cell
+
+def evaluate_private_leaderboard(private_leaderboard_path=private_leaderboard_path):
+    private_leaderboard = pd.read_csv(private_leaderboard_path)
+    new_entries = private_leaderboard.loc[private_leaderboard["calculated_iou"].isna()]
+
+    n = new_entries.shape[0]
+    for i, ix in enumerate(new_entries.index):
+        row = new_entries.loc[i]
+        file_name, alias, dt = row["file_name"], row["alias"], row["date"]
+        print(f"Evaluating model {i+1}/{n} for {alias} submitted at {dt}...")
+        calculated_iou = evaluate_model(f"models_for_evaluation/{file_name}")
+        private_leaderboard.loc[ix, "calculated_iou"] = calculated_iou
+
+    private_leaderboard.to_csv(private_leaderboard_path, index=False)
+    return private_leaderboard
+
+# Internal Cell
+
+def save_public_leaderboard(private_leaderboard_path=private_leaderboard_path, public_leaderboard_path=public_leaderboard_path):
+    private_leaderboard = pd.read_csv(private_leaderboard_path)
+    public_leaderboard = public(private_leaderboard)
+    public_leaderboard.to_csv(public_leaderboard_path, index=False)
+
+    return public_leaderboard
 
 # Cell
 
-def get_leaderboard():
-    df = pd.DataFrame(dict(alias=["davor", "matija", "leptirić"], score=[0.483, 0.453, 0.526]))
 
-    return df.sort_values(by="score", ascending=False).reset_index(drop=True)
+def get_leaderboard(public_leaderboard_path=public_leaderboard_path):
+    public_leaderboard = pd.read_csv(public_leaderboard_path)
+    public_leaderboard = public_leaderboard[(public_leaderboard.alias != "dolphin123") & (public_leaderboard.alias != "malimedo")]
+    return public_leaderboard
